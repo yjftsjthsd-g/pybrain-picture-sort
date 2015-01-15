2c4e88a22fa09eedb76efec07aa71c7dcda0dea5
Brian Cole <yjftsjthsd-g@yahoo.com>
2015-01-14 19:13:38 -0500

    sample images now populate "unknown" images, too, since you need something to test on

--- samples/depopulate.sh
+++ samples/depopulate.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+set -e
+
 #rm ./*/*.jpg
 rm -r whales
 rm -r goats
+rm -r unknown
+rm -r probablyGoats
+rm -r probablyWhales
--- samples/populate.sh
+++ samples/populate.sh
@@ -6,15 +6,22 @@ set -e
 
 mkdir ./whales
 mkdir ./goats
+mkdir ./unknown
+mkdir ./probablyWhales
+mkdir ./probablyGoats
 
 #goats, from https://commons.wikimedia.org/wiki/Category:Goats
 curl https://upload.wikimedia.org/wikipedia/commons/a/a6/A_goat_-_geograph.org.uk_-_1481583.jpg > ./goats/1.jpg &
 curl https://upload.wikimedia.org/wikipedia/commons/5/56/A_Goat.JPG > ./goats/2.jpg &
-curl https://upload.wikimedia.org/wikipedia/commons/5/5b/Chevraux.jpg > ./goats/3.jpg
+curl https://upload.wikimedia.org/wikipedia/commons/5/5b/Chevraux.jpg > ./goats/3.jpg &
 
 #whales, from https://commons.wikimedia.org/wiki/Category:Whales_in_art
-curl https://upload.wikimedia.org/wikipedia/commons/3/3f/BH_Humpback_whale.jpg > ./whales/1.jpg
+curl https://upload.wikimedia.org/wikipedia/commons/3/3f/BH_Humpback_whale.jpg > ./whales/1.jpg &
 curl https://upload.wikimedia.org/wikipedia/commons/6/6d/Orca_Health_Logo.jpeg > ./whales/2.jpg
 curl https://upload.wikimedia.org/wikipedia/commons/e/ef/Wading-pool.jpg > ./whales/3.jpg
 
-#note: yes, the first set is downloaded in parallel and the second is downloaded in sequence. I don't like waiting, but if I do them all in parallel, then the script apparenly can finish before curl and the terminal gets messy. So yes, this is an ugly hack of a fix
+
+curl https://upload.wikimedia.org/wikipedia/commons/5/5d/Kitty_3.jpg > ./unknown/cat.jpg
+curl https://upload.wikimedia.org/wikipedia/commons/f/f3/Baleine_%C3%A0_bosse_et_son_baleineau_-_Humpback_Whale_%26_calf_%28RENO%29.jpg > ./unknown/whale.jpg
+
+#note: yes, some of these are in parallel and some are in sequence. I don't like waiting, but if I do them all in parallel, then the script apparenly can finish before curl and the terminal gets messy. So yes, this is an ugly hack of a fix
